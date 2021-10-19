5bade7d5707ce1cca03dc4437513c6ccacb57cf2
Zelunor <mc.biglenny@gmail.com>
2021-10-19 18:22:28 +0200

    Lua Learning

---
+++ test.lua
@@ -0,0 +1,63 @@
+--print('hello world')
+
+name = 'Lennis'
+number = 31
+nummer2 - 62
+boolean = true
+
+--[[print(name .. 'has' .. number .. 'apples')
+print(number2 - number)
+print(boolean)
+]]
+
+--[[if boolean then 
+    print('True')
+else 
+    print('False')
+end
+
+if number == 31 then
+    print('is correct')
+end
+
+if number2 > 32 then
+    print('Number2 is greater then 31')
+end
+]]
+
+--[[function test()
+    print('Running function')
+end
+
+test()
+
+function test2(x)
+    print("test2:" .. x)
+end
+
+test2(number)
+]]
+
+-- Apples, Grapes, Chex Mix, Ice Cream
+
+--[[array = {}
+-- array = {apples}
+table.insert(array, "Apples",)
+print(array[1])
+table.insert(array, "Grapes")
+print(array[2])
+
+table.remove(array, 1)
+print(array[1])
+]]
+
+--[[for i - 1,20 do
+    print(i)
+end
+
+loop = 0
+while loop <= 10 do
+    print("Loop: " .. loop)
+    loop = loop + 1
+end
+]]
